24f395a4b6ca8fecb0f0629b563fe9213b65f60b
yangmingtian <18516398527@163.com>
2021-01-22 01:27:50 +0800

    dd

---
+++ demo-persistence/src/main/java/com/ymt/demo/persistence/OrderDO.java
@@ -0,0 +1,9 @@
+package com.ymt.demo.persistence;
+
+/**
+ * Created by @author ymtNSN on 2021/1/21
+ */
+public class OrderDO {
+
+
+}
---
+++ demo-persistence/src/main/java/com/ymt/demo/persistence/OrderDataConverter.java
@@ -0,0 +1,17 @@
+package com.ymt.demo.persistence;
+
+import com.ymt.demo.domain.entity.Order;
+
+/**
+ * Created by @author ymtNSN on 2021/1/21
+ */
+public class OrderDataConverter {
+
+    public Order fromData(OrderDO orderDO) {
+        return null;
+    }
+
+    public OrderDO toData(Order order) {
+        return null;
+    }
+}
